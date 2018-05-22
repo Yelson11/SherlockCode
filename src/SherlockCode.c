6695a20c1e6da5b4b2ee325bc120575e80954c38
Yelson <yelsonmonge@gmail.com>
2018-05-22 12:43:40 -0600

    Lectura de archivo.txt

--- src/SherlockCode.c
+++ src/SherlockCode.c
@@ -10,10 +10,26 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include "JavaFileReader.cpp"
 
 int main() {
-	JavaFileReader * j = new JavaFileReader();
-	j->printHola();
+    FILE * archivo;
+    char caracter[100];
+
+    archivo = fopen("D:/Users/Yelson/Desktop/Probabilistic.txt","r");
+
+    if (archivo == NULL)
+        {
+            printf("\nError de apertura del archivo. \n\n");
+        }
+    else {
+    	printf("\nEl contenido del archivo de prueba es \n\n");
+		while (feof(archivo)== 0) {
+			fgets(caracter,100,archivo);
+     		printf("%s",caracter);
+     	}
+		system("PAUSE");
+    }
+	fclose(archivo);
+
 	return 0;
 }
