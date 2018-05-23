4a92ea841789352f1c9fd1dc4a60a7593ddec805
Yelson <yelsonmonge@gmail.com>
2018-05-23 03:36:07 -0600

    Cambio a Sublime Text

---
+++ main.c
@@ -0,0 +1,30 @@
+/*
+ ============================================================================
+ Name        : SherlockCode.c
+ Author      : Yelson Monge
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+//#include "FileReader.c"
+
+int main(/*char *args*/) {
+	printf("Hola mundo");
+    //printf("%s",readFile2(""));
+
+    // llamar a la funcion en FileManager de loadFiles
+
+
+//#pragma in parallel
+  //  inspectRepeated(content); // el tiene forma de hacer copias de chorros de bytes
+    //inspectVariable(content);
+
+//Docker
+//Sublime Text
+
+	return 0;
+}
